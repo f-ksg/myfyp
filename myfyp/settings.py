6dcb4524d3cba2293d24dffc134ceebac332a1e0
f-ksg <82638190+f-ksg@users.noreply.github.com>
2023-02-23 04:27:00 +0800

    added host and fixed secret_key

--- myfyp/settings.py
+++ myfyp/settings.py
@@ -21,12 +21,14 @@ import os
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'myfyp-production.up.railway.app'
+]
 
 
 # Application definition
